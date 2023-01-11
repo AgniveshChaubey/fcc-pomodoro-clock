6e835205c80ef71c09e6bbbfd0e817bff9a4adfe
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-11 18:35:33 +0530

    logic is ready, only break session bug remain to fix

--- src/components/DisplayTime.js
+++ src/components/DisplayTime.js
@@ -5,11 +5,12 @@ import BreakTime from './BreakTime';
 import SessionTime from './SessionTime';
 
 const DisplayTime = () => {
-
-  const [sessionTime, setSessionTime] = useState(25 * 60);
-  const [displayTime, setDisplayTime] = useState(25 * 60);
-  const [breakTime, setBreakTime] = useState(5 * 60);
+  const [sessionTime, setSessionTime] = useState(5);
+  const [displayTime, setDisplayTime] = useState(5);
+  const [breakTime, setBreakTime] = useState(3);
   const [timerOn, setTimerOn] = useState(false);
+  const [onBreak, setOnBreak] = useState(false);
+  const [breakAudio, setBreakAudio] = useState(new Audio('./clockRunning.mp3'));
 
   const formateTime = (time) => {
     var minutes = Math.floor(time / 60);
@@ -20,10 +21,52 @@ const DisplayTime = () => {
     )
   }
 
-  const handlePlayPause = () => {
-    let togglePlayPause = timerOn;
-    return !togglePlayPause;
+  const playSound = () => {
+    breakAudio.currentTime = 0;
+    breakAudio.play();
+    //setTimeout or timeMode
   }
+
+  const handlePlayPause = () => {
+    let second = 1000;
+    let date = new Date().getTime();
+    let nextDate = new Date().getTime() + second;
+    let onBreakVariable = onBreak;
+    
+    if (!timerOn) {
+      let interval = setInterval(() => {
+        date = new Date().getTime();
+        if (date > nextDate) {
+          setDisplayTime(previous => {
+            if (previous <= 0 && !onBreakVariable) {
+              // playSound();
+              onBreakVariable = true;
+              setOnBreak(true);
+              console.log(onBreakVariable);
+              return breakTime;
+            } else if (previous <= 0 && onBreakVariable) {
+              // playSound();
+              onBreakVariable = false;
+              setOnBreak(false);
+              console.log(onBreakVariable);
+              return sessionTime;
+            }
+            return previous - 1;
+          });
+          nextDate =nextDate + second;
+        }
+      }, 30);
+      localStorage.clear();
+      localStorage.setItem('interval-id', interval);
+    }
+    if (timerOn) {
+      clearInterval(localStorage.getItem('interval-id'))
+    }
+    setTimerOn(!timerOn);
+    // console.log(date)
+    // console.log(nextDate)
+  };
+
   const handleReset = () => {
     setBreakTime(5 * 60);
     setSessionTime(25 * 60);
@@ -43,10 +86,11 @@ const DisplayTime = () => {
         />
       </div>
       <div className="timerDisplay">
+      <h3>{ onBreak ? "Break Time" : "Work Time"}</h3>
         <h1>{formateTime(displayTime)}</h1>
         <button onClick={handlePlayPause}>
-          {timerOn ? <FontAwesomeIcon icon={faPlayCircle} /> :
-            <FontAwesomeIcon icon={faCirclePause} />}
+          {timerOn ? <FontAwesomeIcon icon={faCirclePause} /> :
+            <FontAwesomeIcon icon={faPlayCircle} />}
         </button>
         <button className='reset' onClick={handleReset}>
           <FontAwesomeIcon icon={faSync} />
