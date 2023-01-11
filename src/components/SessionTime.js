25bd5ece82710894e549877b9c1fa4fa747ce0a3
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-01-11 09:39:39 +0530

    skleton session time component is ready

--- src/components/SessionTime.js
+++ src/components/SessionTime.js
@@ -7,7 +7,16 @@ const SessionTime = ({ formateTime }) => {
   const [sessionTime, setSessionTime] = useState(25 * 60)
   return (
     <div>
-      
+      <h3>Session Time</h3>
+      <div className="breakClass">
+        <button>
+          <FontAwesomeIcon icon={faLongArrowAltUp} />
+        </button>
+        <h4>{formateTime(sessionTime)}</h4>
+        <button>
+          <FontAwesomeIcon icon={faLongArrowAltUp} />
+        </button>
+      </div>
     </div>
   )
 }
